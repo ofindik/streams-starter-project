c859d45dd3eae654eda1dded23f3f409e3784b83
osmanf <osmanfindik@gmail.com>
2021-03-25 15:56:49 +0300

    Deprecated test methods are updated.

--- src/test/java/com/github/ofindik/udemy/kafka/streams/StreamsStarterAppTest.java
+++ src/test/java/com/github/ofindik/udemy/kafka/streams/StreamsStarterAppTest.java
@@ -1,48 +1,40 @@
 package com.github.ofindik.udemy.kafka.streams;
 
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.TopologyTestDriver;
-import org.apache.kafka.streams.test.ConsumerRecordFactory;
-import org.apache.kafka.streams.test.OutputVerifier;
+import org.apache.kafka.streams.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Properties;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class StreamsStarterAppTest {
 
 	TopologyTestDriver testDriver;
 
-	StringSerializer stringSerializer = new StringSerializer ();
-
-	ConsumerRecordFactory<String, String> recordFactory =
-		new ConsumerRecordFactory<> (stringSerializer, stringSerializer);
-
 	@Before
-	public void setUpTopologyTestDriver(){
-		Properties config = new Properties();
-		config.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
-		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
-		config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-		config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+	public void setUpTopologyTestDriver () {
+		Properties config = new Properties ();
+		config.put (StreamsConfig.APPLICATION_ID_CONFIG, "test");
+		config.put (StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+		config.put (StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String ().getClass ());
+		config.put (StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String ().getClass ());
 
-		StreamsStarterApp streamsStarterApp = new StreamsStarterApp();
-		Topology topology = streamsStarterApp.createTopology();
-		testDriver = new TopologyTestDriver(topology, config);
+		StreamsStarterApp streamsStarterApp = new StreamsStarterApp ();
+		Topology topology = streamsStarterApp.createTopology ();
+		testDriver = new TopologyTestDriver (topology, config);
 	}
 
 	@After
-	public void closeTestDriver(){
-		testDriver.close();
+	public void closeTestDriver () {
+		testDriver.close ();
 	}
 
 	@Test
@@ -53,34 +45,39 @@ public class StreamsStarterAppTest {
 
 	@Test
 	public void makeSureCountsAreCorrect () {
+		TestInputTopic<String, String> inputTopic = testDriver.createInputTopic ("word-count-input",
+			new StringSerializer (), new StringSerializer ());
+		TestOutputTopic<String, Long> outputTopic = testDriver.createOutputTopic ("word-count-output",
+			new StringDeserializer (), new LongDeserializer ());
+
 		String firstExample = "testing Kafka Streams";
-		pushNewInputRecord (firstExample);
-		OutputVerifier.compareKeyValue (readOutput (), "testing", 1L);
-		OutputVerifier.compareKeyValue (readOutput (), "kafka", 1L);
-		OutputVerifier.compareKeyValue (readOutput (), "streams", 1L);
-		assertEquals (readOutput (), null);
+		pushNewInputRecord (inputTopic, firstExample);
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("testing", 1L)));
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("kafka", 1L)));
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("streams", 1L)));
 
 		String secondExample = "testing Kafka again";
-		pushNewInputRecord (secondExample);
-		OutputVerifier.compareKeyValue (readOutput (), "testing", 2L);
-		OutputVerifier.compareKeyValue (readOutput (), "kafka", 2L);
-		OutputVerifier.compareKeyValue (readOutput (), "again", 1L);
+		pushNewInputRecord (inputTopic, secondExample);
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("testing", 2L)));
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("kafka", 2L)));
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("again", 1L)));
 	}
 
 	@Test
-	public void makeSureWordsBecomeLowercase(){
+	public void makeSureWordsBecomeLowercase () {
+		TestInputTopic<String, String> inputTopic = testDriver.createInputTopic ("word-count-input",
+			new StringSerializer (), new StringSerializer ());
+		TestOutputTopic<String, Long> outputTopic = testDriver.createOutputTopic ("word-count-output",
+			new StringDeserializer (), new LongDeserializer ());
+
 		String upperCaseString = "KAFKA kafka Kafka";
-		pushNewInputRecord(upperCaseString);
-		OutputVerifier.compareKeyValue(readOutput(), "kafka", 1L);
-		OutputVerifier.compareKeyValue(readOutput(), "kafka", 2L);
-		OutputVerifier.compareKeyValue(readOutput(), "kafka", 3L);
+		pushNewInputRecord (inputTopic, upperCaseString);
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("kafka", 1L)));
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("kafka", 2L)));
+		assertThat (outputTopic.readKeyValue (), equalTo (new KeyValue<> ("kafka", 3L)));
 	}
 
-	private void pushNewInputRecord (String value) {
-		testDriver.pipeInput (recordFactory.create ("word-count-input", null, value));
-	}
-
-	private ProducerRecord<String, Long> readOutput () {
-		return testDriver.readOutput ("word-count-output", new StringDeserializer (), new LongDeserializer ());
+	private void pushNewInputRecord (TestInputTopic<String, String> inputTopic, String value) {
+		inputTopic.pipeInput (null, value);
 	}
 }
